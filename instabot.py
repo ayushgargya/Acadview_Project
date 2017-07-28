73f8392a4b67bac0db3bf5cea57356068d3428c7
Ayush Gargya <ayush.gargya@gmail.com>
2017-07-28 22:41:19 +0530

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -6,15 +6,12 @@ from wordcloud import WordCloud,STOPWORDS
 from token_key import APP_ACCESS_TOKEN
 
 BASE_URL = 'https://api.instagram.com/v1/'
-
-
-##############instabot 1.0 ######################
-#function to get self info of the user
+##############instabot 1.0###############
+#function to get ur info(username,followers,following etc..
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
+    print 'GET request url : %s' % (request_url)len()
     user_info = requests.get(request_url).json()
-
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
             print 'Username: %s' % (user_info['data']['username'])
@@ -25,26 +22,21 @@ def self_info():
             print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
+#####################################################
 
-####################################################
-
-#function to get the id of user from the username
+#to get the id of user from the username
 def get_user_id(insta_username):
     request_url = (BASE_URL + "users/search?q=%s&access_token=%s" % (insta_username, APP_ACCESS_TOKEN))
     print"Get Request Url : %s"%(request_url)
-    #to convert to data only
     user_info=requests.get(request_url).json()
     if user_info["meta"]["code"]==200:
         if len(user_info["data"]):
-            #to get 1st user in case of same name
+            #to return the first search user.
             return user_info["data"][0]["id"]
         else:
             return None
     else:
         print"Status other than 200 received !!!!!"
-
-####################################################
-
 #to get details of a user
 def get_user_info(insta_username):
     user_id = get_user_id(insta_username)
@@ -54,7 +46,6 @@ def get_user_info(insta_username):
     request_url = (BASE_URL + "users/%s?access_token=%s") % (user_id, APP_ACCESS_TOKEN)
     print "GET request url : %s" % (request_url)
     user_info = requests.get(request_url).json()
-
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
             print "Username: %s" % (user_info['data']['username'])
@@ -65,15 +56,12 @@ def get_user_info(insta_username):
             print "There is no data for this user!"
     else:
         print "Status code other than 200 received!"
-
-###################################################
-
-#to get your own recent post
+####################################################
+        #to get your own recent post
 def get_own_post():
     request_url = (BASE_URL + "users/self/media/recent/?access_token=%s") % (APP_ACCESS_TOKEN)
     print "GET request url : %s" % (request_url)
     own_media = requests.get(request_url).json()
-
     if own_media["meta"]["code"] == 200:
         if len(own_media["data"]):
             image_name = own_media["data"][0]["id"] + ".jpeg"
@@ -85,10 +73,8 @@ def get_own_post():
             print "Post does not exist!"
     else:
         print "Status code other than 200 received!"
-
-######################################################
-
-#function to get user's recent post
+###############################################################3
+#to get one's recent post
 def get_user_post(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
@@ -97,7 +83,6 @@ def get_user_post(insta_username):
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_media = requests.get(request_url).json()
-
     if user_media['meta']['code'] == 200:
         if len(user_media['data']):
             print "POST ID : %s" + (user_media['data'][0]['id'])
@@ -109,9 +94,7 @@ def get_user_post(insta_username):
             print 'Post does not exist!'
     else:
         print 'Status code other than 200 received!'
-
-#######################################################
-
+        ##################################################3
 #function to get post id of recent post of user
 def get_post_id(insta_username):
     user_id = get_user_id(insta_username)
@@ -121,10 +104,8 @@ def get_post_id(insta_username):
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_media = requests.get(request_url).json()
-
     if user_media['meta']['code'] == 200:
         if len(user_media['data']):
-
             return user_media['data'][0]['id']
         else:
             print 'There is no recent post of the user!'
@@ -132,10 +113,8 @@ def get_post_id(insta_username):
     else:
         print 'Status code other than 200 received!'
         exit()
-
-######################################################
-
-#to post a like on recent post of user
+        ##################################################3
+#to post a like on recent post of a user
 def like_a_post(insta_username):
     media_id = get_post_id(insta_username)
     request_url = (BASE_URL + 'media/%s/likes') % (media_id)
@@ -143,13 +122,12 @@ def like_a_post(insta_username):
     print 'POST request url : %s' % (request_url)
     post_a_like = requests.post(request_url, payload).json()
     if post_a_like['meta']['code'] == 200:
-        print 'Like added!'
+        print 'Like was successful!'
     else:
         print 'Your like was unsuccessful. Try again!'
 
-########################################################
-
-#to get post id of own recent post
+#########################################################
+#function to get post id of own recent post
 def get_own_post_id():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -162,14 +140,14 @@ def get_own_post_id():
             print 'Post does not exist!'
     else:
         print 'Status code other than 200 received!'
+#######################################################
 
-#to post a comment on recent post of a user
+#function to post a comment on recent post of a user
 def post_a_comment(insta_username):
     media_id = get_post_id(insta_username)
     comment_text = raw_input("Your comment: ")
     if len(comment_text) <= 300 and comment_text.isupper()==False and comment_text.count("#")<=4:
         c = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',comment_text)
-
         if len(c) <= 1:
             payload = {"access_token": APP_ACCESS_TOKEN, "text": comment_text}
             request_url = (BASE_URL + 'media/%s/comments') % (media_id)
@@ -181,13 +159,14 @@ def post_a_comment(insta_username):
                 print "Unable to add comment. Try again!"
 
         else:
-            print "Comment cannot be posted not appropriate"
+            print "Comment cannot be posted as it is not appropriate"
 
     else:
         print "Comment cannot be posted as it is not appropriate"
-######################################################################
 
-#to get comments info on friends recent post
+        ######################################################
+
+#to post comments info on friends recent post
 def comment_info(insta_username):
     get_user_id(insta_username)
     media_id = get_post_id(insta_username)
@@ -204,9 +183,7 @@ def comment_info(insta_username):
             print "no data"
     else:
         print"code not 200"
-
-####################################################
-
+        #######################################################
 #get comments of own recent post
 def own_comment_info():
     media_id = get_own_post_id()
@@ -224,9 +201,7 @@ def own_comment_info():
     else:
         print"code not 200"
 
-######################################################
-
-#to delete -ive comments from post
+#to delete negative comments on post
 def delete_negative_comment(insta_username):
     media_id = get_post_id(insta_username)
     request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
@@ -255,11 +230,10 @@ def delete_negative_comment(insta_username):
             print 'There are no existing comments on the post!'
     else:
         print 'Status code other than 200 received!'
+        
+#################################################################
 
-#############################################################################
-
-#objective 1 negative vs positive comments
-#function to compare the comments on own recent post nd plot a pie chart of same
+#to compare the comments on own recent post & plot a pie chart of same
 def compare_comments():
     media_id = get_own_post_id()
     request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
@@ -280,32 +254,22 @@ def compare_comments():
             else:
                 print "Positive comment : %s by %s\n" % (comment_text,comment_info["data"][x]["from"]["username"])
                 positive=positive+1
-
-
-        print"positive comments : %s"%(positive)
-        print"negative comments : %s"%(negative)
+        print"GOOD comments :) : %s"%(positive)
+        print"BAD comments :( : %s"%(negative)
         labels = "Positive Comments", "Negative Comments"
         numbers = [positive, negative]
         colors = ['blue', 'red']
-        explode = (0.1, 0)  # explode 1st slice
-        # Plot
+        explode = (0.1, 0) 
         plt.pie(numbers, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
-
         plt.axis('equal')
         plt.show()
-#end of objective 1 :>
-
-##############################################################3
-
-#objective no 2 :
-#functions target a particular tag and comment a appropriate comment
+        ##################################################################
+#objective 1:
+#To target a particular tag & comment an appropriate comment of your choice to the meadia of that tag
 def post_a_targetted_comment(media_id):
-
     comment_text =raw_input("Enter the comment you want to post? \n")
-
     if len(comment_text) <= 300 and comment_text.isupper()==False and comment_text.count("#")<=4:
         c = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',comment_text)
-
         if len(c) <= 1:
             payload = {"access_token": APP_ACCESS_TOKEN, "text": comment_text}
             request_url = (BASE_URL + 'media/%s/comments') % (media_id)
@@ -333,18 +297,31 @@ def get_media_of_tag(tag):
             print "There is no recent post!"
     else:
         print "Status code other than 200 received!"
+        
+#################################################################
 
-#end of objective 2:>
-
-###############################################################
-
+#function to show recently liked media
+def recent_media_liked():
+    request_url = BASE_URL + "users/self/media/liked?access_token=%s" % (APP_ACCESS_TOKEN)
+    recently_liked_media=requests.get(request_url).json()
+    if recently_liked_media["meta"]["code"] == 200:
+        if len(recently_liked_media["data"]):
+            image_name = recently_liked_media["data"][0]["id"] + ".jpeg"
+            image_url = recently_liked_media["data"][0]["images"]["standard_resolution"]["url"]
+            urllib.urlretrieve(image_url, image_name)
+            print "Your image has been downloaded!"
+        else:
+            print "User does not exist!"
+    else:
+        print "Status code other than 200 received!"
+        #########################################################################
 #function to download the posts of user or self
 def download_recent_posts():
     a=True
     while a==True:
         print "\n"
-        print "a.Download post"
-        print "b. Download other's recent post"
+        print "a.Download own  posts"
+        print "b. Download other user's post"
         choice = raw_input("enter the choice:")
 
         if choice=="a":
@@ -396,10 +373,7 @@ def download_recent_posts():
         else:
             print "Wrong Choice"
             a = False
-
-##################################################################3333
-
-#objective 3
+###############################################################################
 #function to find subtrends of a post
 def find_subtrends(tag):
     request_url=BASE_URL+ "tags/%s/media/recent?access_token=%s"%(tag,APP_ACCESS_TOKEN)
@@ -415,8 +389,6 @@ def find_subtrends(tag):
                         hash_items[user_media['data'][x]['tags'][y]] += 1
                     else:
                         hash_items[user_media['data'][x]['tags'][y]] = 1
-
-
         else:
             print "There is no recent post!"
     else:
@@ -429,11 +401,9 @@ def find_subtrends(tag):
     pylab.xticks(x, hash_items.keys())
     pylab.plot(x, hash_items.values(), "g")
     pylab.show()
-
-
     wordcloud = WordCloud(font_path=r"C:\Windows\Fonts\FREESCPT.TTF",
                       stopwords=STOPWORDS,
-                      background_color="blue",
+                      background_color="white",
                       width=1200,
                       height=1000,
                       ).generate_from_frequencies(hash_items)
@@ -441,19 +411,9 @@ def find_subtrends(tag):
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     plt.show()
-#end of objective 3:>
-
-###############################################################
-
-#objective 4 to caption analyse calamity
+############################end of objective :>##################################
+#Function to cleck climate and return if any natural disaster#
 def climate_check(latitude, longitude):
-    """
-    1.URL for location
-    2.Make the request
-    3.Get media id
-    4.Analyse captions
-    5.Check for calamity
-    """
     url = (BASE_URL + "locations/search?lat=%s&lng=%s&access_token=%s") % (latitude, longitude, ACCESS_TOKEN)
     location_info = requests.get(url).json()
     if location_info['meta']['code'] == 200:
@@ -499,25 +459,12 @@ def climate_check(latitude, longitude):
     else:
         print "Something went wrong!"
 
-#end of objective 4 :>
-##############################################################
+        ###########################################################################
 
-def chart_example():
-
-    labels = 'cats','cake','pizza','r&b'
-    sizes = [40, 25, 35, 30]
-    explode = (0, 0, 0.1, 0)  # only "explode" the 3rd slice
-
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.show()
-################################################################
 def start_bot():
     while True:
         print "\n"
-        print "Hey! Welcome to instaBot 1.0!"
+        print "Hey! Welcome to instaBot!"
         print "Choose from following  options:"
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
@@ -527,13 +474,13 @@ def start_bot():
         print "f. comment on recent media of user\n"
         print "g. view comments on own recent post \n"
         print "h. view comments on user's recent post \n"
-        print "i. delete nagative comments on the picture \n"
-        print "j. negative vs positive comment and  a pie chart \n"
-        print "k. Target a particular post and do comment \n"
-        print "l. Download the recent media of anyone \n"
-        print "m. to find and plot subtrends of a trend \n"
-        print "n.Draw a Example Pie-chart"
-        print "o. Calamity analysis between ant latitudes \n"
+        print "i. delete bad comments on the picture \n"
+        print "j. compare the comments on own recent post an creat a pie chart of the same\n"
+        print "k. Target a particular tag and comment a appropriate comment of ur wish to the media of that tag\n"
+        print "l. Download the recent media you just liked \n"
+        print "m. Download the recent media of anyone \n"
+        print "n. to find and plot subtrends of a trend \n"
+        print "o. Calamity analysis between any latitudes \n"
         print "p.Exit"
 
         choice=raw_input("Enter you choice: ")
@@ -600,8 +547,9 @@ def start_bot():
             climate_check(latitude, longitude)
         elif choice=="p":
             exit()
+
         else:
             print "wrong choice"
 
 start_bot()
-
+########End of instabot########
