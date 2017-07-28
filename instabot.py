0e6b79290376e7b25aa3bfd64894f3b9f9579dc0
Ayush Gargya <ayush.gargya@gmail.com>
2017-07-28 22:50:34 +0530

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -10,7 +10,7 @@ BASE_URL = 'https://api.instagram.com/v1/'
 #function to get ur info(username,followers,following etc..
 def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)len()
+    print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
